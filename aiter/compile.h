e33a5ff3f8b7f7db82dfd26f806f59524198b584
fsx950223 <fsx950223@outlook.com>
2025-03-14 10:59:40 +0000

    fix a bug

--- aiter/compile.h
+++ aiter/compile.h
@@ -6,9 +6,9 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#endif
-
 void unload_{kernel_name}(void);
 void load_{kernel_name}(void);
 // tt-linker: {kernel_name}:{full_signature}:{algo_info}
 hipError_t{_placeholder} {kernel_name}(hipStream_t stream, {signature});
+
+#endif
