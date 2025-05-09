5d3d25cbf18f4793eeef24c1655d4ce8ae14b45d
Aliasger Zaidy <aliasger.zaidy@amd.com>
2025-05-09 13:35:54 +0000

    Added new workaround

--- aiter/ops/triton/mha.py
+++ aiter/ops/triton/mha.py
@@ -205,12 +205,18 @@ def _attn_fwd_inner(
 ):
     RCP_LN2: tl.constexpr = 1.4426950408889634
 
-    # Workaround for int64 strides
-    dummy_M = 10
-
-    stride_kn = dummy_M.to(tl.int64) * 0 + stride_kn_in
-    stride_vk = dummy_M.to(tl.int64) * 0 + stride_vk_in
-    stride_sn = dummy_M.to(tl.int64) * 0 + stride_sn_in
+    # NOTE:
+    # Workaround for int64 strides, In the absence of strides being int64 parts of offset
+    # computation is done in 32 bit and overflows resulting in segfaults
+    # If input strides are defined as int64, it disables vectorized loads which drops perf
+    # If we define new strides as stride_x = stride_x_in.to(tl.int64), that does not work
+    # because strides are tl.constexpr and cannot be upcasted
+    # If we define new strides as stride_x: tl.int64 = stride_x_in, segfault remains
+    # The permanent solution is to enable upcasting of tl.constexpr
+    # In the meantime, the following workaround provides correctness and does not drop perf
+    stride_kn = tl.cast(stride_kn_in, tl.int64)
+    stride_vk = tl.cast(stride_vk_in, tl.int64)
+    stride_sn = tl.cast(stride_sn_in, tl.int64)
 
     # loop over k, v, and update accumulator
 
@@ -378,37 +384,43 @@ def _attn_fwd(q_ptr: torch.Tensor,
     offs_n = tl.arange(0, BLOCK_N).to(tl.int64)
     offs_d = tl.arange(0, BLOCK_DMODEL_POW2).to(tl.int64)
 
-    # Workaround for int64 strides
-    dummy_M = 10
-
-    stride_qz           = dummy_M.to(tl.int64) * 0 + stride_qz_in
-    stride_qh           = dummy_M.to(tl.int64) * 0 + stride_qh_in
-    stride_qm           = dummy_M.to(tl.int64) * 0 + stride_qm_in
-    stride_qk           = dummy_M.to(tl.int64) * 0 + stride_qk_in
-    stride_kz           = dummy_M.to(tl.int64) * 0 + stride_kz_in
-    stride_kh           = dummy_M.to(tl.int64) * 0 + stride_kh_in
-    stride_kn           = dummy_M.to(tl.int64) * 0 + stride_kn_in
-    stride_kk           = dummy_M.to(tl.int64) * 0 + stride_kk_in
-    stride_vz           = dummy_M.to(tl.int64) * 0 + stride_vz_in
-    stride_vh           = dummy_M.to(tl.int64) * 0 + stride_vh_in
-    stride_vn           = dummy_M.to(tl.int64) * 0 + stride_vn_in
-    stride_vk           = dummy_M.to(tl.int64) * 0 + stride_vk_in
-    stride_descale_q_z  = dummy_M.to(tl.int64) * 0 + stride_descale_q_z_in
-    stride_descale_k_z  = dummy_M.to(tl.int64) * 0 + stride_descale_k_z_in
-    stride_descale_v_z  = dummy_M.to(tl.int64) * 0 + stride_descale_v_z_in
-    stride_oz           = dummy_M.to(tl.int64) * 0 + stride_oz_in
-    stride_oh           = dummy_M.to(tl.int64) * 0 + stride_oh_in
-    stride_om           = dummy_M.to(tl.int64) * 0 + stride_om_in
-    stride_on           = dummy_M.to(tl.int64) * 0 + stride_on_in
-    stride_alibi_z      = dummy_M.to(tl.int64) * 0 + stride_alibi_z_in
-    stride_alibi_h      = dummy_M.to(tl.int64) * 0 + stride_alibi_h_in
-    stride_sd_z         = dummy_M.to(tl.int64) * 0 + stride_sd_z_in
-    stride_sd_h         = dummy_M.to(tl.int64) * 0 + stride_sd_h_in
-    stride_sd_m         = dummy_M.to(tl.int64) * 0 + stride_sd_m_in
-    stride_sd_n         = dummy_M.to(tl.int64) * 0 + stride_sd_n_in
-    stride_lse_z        = dummy_M.to(tl.int64) * 0 + stride_lse_z_in
-    stride_lse_h        = dummy_M.to(tl.int64) * 0 + stride_lse_h_in
-    stride_lse_m        = dummy_M.to(tl.int64) * 0 + stride_lse_m_in
+    # NOTE:
+    # Workaround for int64 strides, In the absence of strides being int64 parts of offset
+    # computation is done in 32 bit and overflows resulting in segfaults
+    # If input strides are defined as int64, it disables vectorized loads which drops perf
+    # If we define new strides as stride_x = stride_x_in.to(tl.int64), that does not work
+    # because strides are tl.constexpr and cannot be upcasted
+    # If we define new strides as stride_x: tl.int64 = stride_x_in, segfault remains
+    # The permanent solution is to enable upcasting of tl.constexpr
+    # In the meantime, the following workaround provides correctness and does not drop perf
+    stride_qz           = tl.cast(stride_qz_in, tl.int64)
+    stride_qh           = tl.cast(stride_qh_in, tl.int64)
+    stride_qm           = tl.cast(stride_qm_in, tl.int64)
+    stride_qk           = tl.cast(stride_qk_in, tl.int64)
+    stride_kz           = tl.cast(stride_kz_in, tl.int64)
+    stride_kh           = tl.cast(stride_kh_in, tl.int64)
+    stride_kn           = tl.cast(stride_kn_in, tl.int64)
+    stride_kk           = tl.cast(stride_kk_in, tl.int64)
+    stride_vz           = tl.cast(stride_vz_in, tl.int64)
+    stride_vh           = tl.cast(stride_vh_in, tl.int64)
+    stride_vn           = tl.cast(stride_vn_in, tl.int64)
+    stride_vk           = tl.cast(stride_vk_in, tl.int64)
+    stride_descale_q_z  = tl.cast(stride_descale_q_z_in, tl.int64)
+    stride_descale_k_z  = tl.cast(stride_descale_k_z_in, tl.int64)
+    stride_descale_v_z  = tl.cast(stride_descale_v_z_in, tl.int64)
+    stride_oz           = tl.cast(stride_oz_in, tl.int64)
+    stride_oh           = tl.cast(stride_oh_in, tl.int64)
+    stride_om           = tl.cast(stride_om_in, tl.int64)
+    stride_on           = tl.cast(stride_on_in, tl.int64)
+    stride_alibi_z      = tl.cast(stride_alibi_z_in, tl.int64)
+    stride_alibi_h      = tl.cast(stride_alibi_h_in, tl.int64)
+    stride_sd_z         = tl.cast(stride_sd_z_in, tl.int64)
+    stride_sd_h         = tl.cast(stride_sd_h_in, tl.int64)
+    stride_sd_m         = tl.cast(stride_sd_m_in, tl.int64)
+    stride_sd_n         = tl.cast(stride_sd_n_in, tl.int64)
+    stride_lse_z        = tl.cast(stride_lse_z_in, tl.int64)
+    stride_lse_h        = tl.cast(stride_lse_h_in, tl.int64)
+    stride_lse_m        = tl.cast(stride_lse_m_in, tl.int64)
 
     if VARLEN:
         cu_seqlens_q_start = tl.load(cu_seqlens_q + off_z)
