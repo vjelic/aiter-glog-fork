1f5d8dc29775739cb66c9205249311d7dd635517
lalala-sh <Jiaxing.Wen@amd.com>
2025-05-14 03:34:00 -0500

    fix

--- csrc/py_itfs_ck/moe_sorting_kernels.cu
+++ csrc/py_itfs_ck/moe_sorting_kernels.cu
@@ -30,7 +30,7 @@ void moe_sorting_fwd(torch::Tensor &topk_ids,          // [m, topk]
     const at::cuda::OptionalCUDAGuard device_guard(device_of(topk_ids));
     const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-    int workspace_size = moe_sorting_get_workspace_size(num_tokens, num_experts, topk);
+    int workspace_size = moe_sorting_get_workspace_size(num_tokens, num_experts);
     void *ws_ptr = nullptr;
     if (workspace_size > 0)
     {
