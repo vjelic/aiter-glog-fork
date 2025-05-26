443d4dbaab1798ffdd1c7dd79f594cb93010a249
chenjun <junchen2@amd.com>
2025-05-26 14:32:57 +0000

    optimize quant1

--- csrc/kernels/quant_kernels.cu
+++ csrc/kernels/quant_kernels.cu
@@ -8,7 +8,7 @@
 
 #include <hipcub/hipcub.hpp>
 #include "vec_convert.h"
-#include "rocprim/rocprim.hpp"
+#include <rocprim/rocprim.hpp>
 
 const int32_t BlockSize = 256;
 const int32_t groupQuantBlockSize = 64;
@@ -94,10 +94,10 @@ namespace aiter
   }
 
   template <typename DTYPE_I, typename DTYPE_O>
-  __device__ float data_to_per_row_scale(const DTYPE_I *__restrict__ input,
+  __device__ inline std::tuple<float, DTYPE_I*> data_to_per_row_scale(const DTYPE_I *__restrict__ input,
                                          int cols)
   {
-    static constexpr int32_t vec_size_i = 16 / sizeof(DTYPE_O);
+    static constexpr int32_t vec_size_i = thread_data_size;
     static constexpr int32_t vec_size_o = std::is_same_v<DTYPE_O, ck_tile::fp4x2_t> ? vec_size_i / 2 : vec_size_i;
     using vec_i = ck_tile::vec_t<DTYPE_I, vec_size_i>;
     using tb_i = ck_tile::thread_buffer<DTYPE_I, vec_size_o>;
@@ -106,8 +106,8 @@ namespace aiter
     const int64_t row_offset = blockIdx.x * cols;
     auto const *input_ptr = reinterpret_cast<DTYPE_I const *>(input);
     auto const *input_vecs = reinterpret_cast<vec_i const *>(input + row_offset);
-    auto buffer_i = ck_tile::make_buffer_view<ck_tile::address_space_enum::global>(input_ptr, gridDim.x * cols);
-    buffer_i.init_raw();
+    // auto buffer_i = ck_tile::make_buffer_view<ck_tile::address_space_enum::global>(input_ptr, gridDim.x * cols);
+    // buffer_i.init_raw();
 
     // double load core loop start
     const int32_t num_elems_tail = cols % vec_size_i;
@@ -169,7 +169,129 @@ namespace aiter
       if (((u32 & 0x400000)) && (((u32 & 0x200000)) || ((u32 & 0x1FFFFF)) || (exponent)))
         exponent+=1;
       return ck_tile::bit_cast<float>(exponent << 23); };
-    return std::is_same_v<DTYPE_O, ck_tile::fp4x2_t> ? fp4_scale(absMax) * inverted_DTYPE_MAX : absMax * inverted_DTYPE_MAX;
+    float row_scale = std::is_same_v<DTYPE_O, ck_tile::fp4x2_t> ? fp4_scale(absMax) * inverted_DTYPE_MAX : absMax * inverted_DTYPE_MAX;
+    return std::make_tuple(row_scale, reinterpret_cast<DTYPE_I *>(&vec_cur));
+  }
+
+
+  template <typename vec_i, typename DTYPE_I, int vec_size_i>
+  __device__ inline void buffle_load(vec_i* thread_data_ptr, DTYPE_I* input_ptr, int load_size=vec_size_i)
+  {
+    using vec_8 = ck_tile::vec_t<DTYPE_I, 8>;
+    using vec_4 = ck_tile::vec_t<DTYPE_I, 4>;
+    using vec_2 = ck_tile::vec_t<DTYPE_I, 2>;
+    using vec_1 = ck_tile::vec_t<DTYPE_I, 1>;
+
+    if (vec_size_i == load_size)
+    {
+      *thread_data_ptr = reinterpret_cast<vec_i *>(input_ptr)[0];
+      return;
+    }
+
+    auto *data_ptr_8 = reinterpret_cast<vec_8 *>(thread_data_ptr);
+
+    for(int j=0; j<((load_size)>>3&1); j++)
+    {
+      *data_ptr_8 = reinterpret_cast<vec_8 *>(input_ptr)[0];
+      data_ptr_8++;
+      input_ptr += 8;
+    }
+
+    auto *data_ptr_4 = reinterpret_cast<vec_4 *>(data_ptr_8);
+    for(int j=0; j<((load_size)>>2&1); j++)
+    {
+      *data_ptr_4 = reinterpret_cast<vec_4 *>(input_ptr)[0];
+      data_ptr_4++;
+      input_ptr += 4;
+    }
+
+    auto *data_ptr_2 = reinterpret_cast<vec_2 *>(data_ptr_4);
+    for(int j=0; j<((load_size)>>1&1); j++)
+    {
+      *data_ptr_2 = reinterpret_cast<vec_2 *>(input_ptr)[0];
+      data_ptr_2++;
+      input_ptr += 2;
+    }
+
+    if(load_size&1)
+    {
+      auto *data_ptr_1 = reinterpret_cast<DTYPE_I *>(data_ptr_2);
+      *data_ptr_1 = *input_ptr;
+    }
+  }
+
+  template <typename DTYPE_I, typename DTYPE_O>
+  __device__ inline std::tuple<float, DTYPE_I*> data_to_per_row_scale_retain_vgpr(DTYPE_I *input,
+                                         int cols)
+  {
+    static constexpr int32_t vec_size_i = thread_data_size;
+    static constexpr int32_t vec_size_o = std::is_same_v<DTYPE_O, ck_tile::fp4x2_t> ? vec_size_i / 2 : vec_size_i;
+    using vec_i = ck_tile::vec_t<DTYPE_I, vec_size_i>;
+    const float inverted_DTYPE_MAX = std::is_same_v<DTYPE_O, ck_tile::fp4x2_t> ? 0.25 : (1. / ck_tile::type_convert<float>(ck_tile::numeric<DTYPE_O>::max()));
+
+    const int64_t row_offset = blockIdx.x * cols;
+    auto *input_ptr = reinterpret_cast<DTYPE_I *>(input + row_offset);
+    auto const *input_vecs = reinterpret_cast<vec_i const *>(input_ptr);
+
+    int32_t real_thread_data_size = cols / BlockSize;
+    const int32_t num_elems_tail = cols - real_thread_data_size * BlockSize;
+    input_ptr += thread_data_size * threadIdx.x;
+    if (threadIdx.x < num_elems_tail)
+    {
+      real_thread_data_size += 1;
+      input_ptr += threadIdx.x;
+    }
+    else{
+      input_ptr += num_elems_tail;
+    }
+
+    // using tb_i = ck_tile::thread_buffer<DTYPE_I, vec_size_i>;
+    // auto buffer_i = ck_tile::make_buffer_view<ck_tile::address_space_enum::global>(input_ptr, gridDim.x * cols);
+    // buffer_i.init_raw();
+
+    // vec_i vec_cur = ck_tile::bit_cast<vec_i>(buffer_i.template get<tb_i>(threadIdx.x* vec_size_i, row_offset, true));
+
+    
+    // vec_i vec_cur = input_vecs[threadIdx.x];
+    vec_i vec_cur;
+    auto *vec_ptr = reinterpret_cast<DTYPE_I*>(&vec_cur);
+    // buffle_load<vec_i, DTYPE_I, thread_data_size>(&vec_cur, input_ptr, real_thread_data_size);
+    
+    
+    for (size_t j = 0; j < thread_data_size; j++)
+    {
+      if (j < real_thread_data_size)
+      {
+        vec_ptr[j] = input_ptr[j];
+      }
+    }
+    // __syncthreads();
+
+    float absMax = 0.0f;
+    for (size_t j = 0; j < thread_data_size; j++)
+    {
+      if (j < real_thread_data_size)
+      {
+        absMax = max(absMax, abs(ck_tile::type_convert<float>(vec_cur[j])));
+      }
+    }
+
+    using BlockReduce = hipcub::BlockReduce<float, BlockSize>;
+    __shared__ typename BlockReduce::TempStorage temp_storage;
+    absMax = BlockReduce(temp_storage).Reduce(absMax, hipcub::Max());
+
+    auto fp4_scale = [](float tmp)
+    {uint32_t u32= ck_tile::bit_cast<uint32_t>(tmp);
+      uint32_t exponent = (u32 >> 23) & 0b11111111;
+      if (exponent == 0b11111111)
+      {
+        return ck_tile::bit_cast<float>(exponent<<23);
+      }
+      if (((u32 & 0x400000)) && (((u32 & 0x200000)) || ((u32 & 0x1FFFFF)) || (exponent)))
+        exponent+=1;
+      return ck_tile::bit_cast<float>(exponent << 23); };
+    float row_scale = std::is_same_v<DTYPE_O, ck_tile::fp4x2_t> ? fp4_scale(absMax) * inverted_DTYPE_MAX : absMax * inverted_DTYPE_MAX;
+    return std::make_tuple(row_scale, reinterpret_cast<DTYPE_I *>(&vec_cur));
   }
 
   template <typename DTYPE_I, typename DTYPE_O>
@@ -177,7 +299,8 @@ namespace aiter
                                        const DTYPE_I *__restrict__ input,
                                        int cols)
   {
-    float row_scale = data_to_per_row_scale<DTYPE_I, DTYPE_O>(input, cols);
+    auto res = data_to_per_row_scale<DTYPE_I, DTYPE_O>(input, cols);
+    float row_scale = std::get<0>(res);
     if (threadIdx.x == 0)
     {
       vllm::atomicMaxFloat(scale, row_scale);
@@ -186,7 +309,7 @@ namespace aiter
 
   template <typename DTYPE_I, typename DTYPE_O>
   __device__ void scaled_quant_impl(DTYPE_O *__restrict__ out,
-                                    const DTYPE_I *__restrict__ input,
+                                    DTYPE_I * input,
                                     const float *__restrict__ scale,
                                     int cols)
   {
@@ -256,9 +379,34 @@ namespace aiter
       }
     }
   }
+
+  template <typename DTYPE_I, typename DTYPE_O>
+  __device__ void scaled_quant_vgpr_impl(DTYPE_O *__restrict__ out,
+                                    DTYPE_I * input,
+                                    const float *__restrict__ scale,
+                                    int cols)
+  {
+
+    const float inverted_scale = std::is_same_v<DTYPE_O, ck_tile::fp4x2_t> ? (*scale) : 1.0f / (*scale);
+    static constexpr int32_t vec_size_i = thread_data_size;
+    static constexpr int32_t vec_size_o = std::is_same_v<DTYPE_O, ck_tile::fp4x2_t> ? vec_size_i / 2 : vec_size_i;
+
+    using vec_i = ck_tile::vec_t<DTYPE_I, vec_size_i>;
+    using vec_o = ck_tile::vec_t<DTYPE_O, vec_size_o>;
+
+    const int64_t row_offset = blockIdx.x * cols;
+    auto const *input_vecs = reinterpret_cast<vec_i const *>(input);
+    auto *out_ptr = reinterpret_cast<DTYPE_O *>(out);
+    auto *out_vecs = std::is_same_v<DTYPE_O, ck_tile::fp4x2_t> ? reinterpret_cast<vec_o *>(out + row_offset / 2) : reinterpret_cast<vec_o *>(out + row_offset);
+
+    vec_i vec_cur = *input_vecs;
+    out_vecs[threadIdx.x] = ck_tile::vec_convert<DTYPE_O, DTYPE_I, vec_size_i>(vec_cur, inverted_scale);
+  }
+
+
   template <typename DTYPE_I, typename DTYPE_O>
   __global__ void scaled_quant_kernel(DTYPE_O *__restrict__ out,
-                                      const DTYPE_I *__restrict__ input,
+                                      DTYPE_I * input,
                                       const float *__restrict__ scale,
                                       int cols)
   {
@@ -268,19 +416,31 @@ namespace aiter
   template <typename DTYPE_I, typename DTYPE_O>
   __global__ void dynamic_per_token_scaled_quant_kernel(
       DTYPE_O *__restrict__ out, float *__restrict__ scale,
-      DTYPE_I const *__restrict__ input, float const *__restrict__ scale_ub,
+      DTYPE_I * input, float const *__restrict__ scale_ub,
       const int32_t cols)
   {
     // float const min_scaling_factor = 1.0f / (FP8_MAX * 512.f);
 
     const int token_idx = blockIdx.x;
-    float row_scale = data_to_per_row_scale<DTYPE_I, DTYPE_O>(input, cols);
+    float row_scale;
+    DTYPE_I* vec_ptr;
+    // if ((cols / BlockSize) > thread_data_size)
+    // {
+      // auto res = data_to_per_row_scale<DTYPE_I, DTYPE_O>(input, cols);
+      //  row_scale = std::get<0>(res);
+    // }
+    // else
+    // {
+      auto res = data_to_per_row_scale_retain_vgpr<DTYPE_I, DTYPE_O>(input, cols);
+      row_scale = std::get<0>(res);
+      vec_ptr = std::get<1>(res);
+    // }
 
     __shared__ float token_scale;
     if (threadIdx.x == 0)
     {
       token_scale = row_scale;
-      if (std::is_same_v<DTYPE_O, ck_tile::fp4x2_t>)
+      if constexpr (std::is_same_v<DTYPE_O, ck_tile::fp4x2_t>)
       {
         // scale[token_idx] = token_scale;
         auto *tmp = reinterpret_cast<uint8_t *>(scale);
@@ -294,7 +454,14 @@ namespace aiter
     }
     __syncthreads();
 
-    scaled_quant_impl<DTYPE_I>(out, input, &token_scale, cols);
+    // if ((cols / BlockSize) > thread_data_size)
+    // {
+    //   scaled_quant_impl<DTYPE_I>(out, input, &token_scale, cols);
+    // }
+    // else
+    // {
+      scaled_quant_vgpr_impl<DTYPE_I>(out, vec_ptr, &token_scale, cols);
+    // }
   }
 
 void static_per_tensor_quant(torch::Tensor &out,         // [..., d]
