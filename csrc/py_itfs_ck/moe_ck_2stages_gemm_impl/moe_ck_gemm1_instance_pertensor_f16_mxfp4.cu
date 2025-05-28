1428aa6dd7577a1257438b7958e5a56ede6b4e13
root <root@bg-1w300-g1-2a.mkm.dcgpu>
2025-05-28 11:31:03 +0000

    revert me after codegen is ready

--- csrc/py_itfs_ck/moe_ck_2stages_gemm_impl/moe_ck_gemm1_instance_pertensor_f16_mxfp4.cu
+++ csrc/py_itfs_ck/moe_ck_2stages_gemm_impl/moe_ck_gemm1_instance_pertensor_f16_mxfp4.cu
@@ -18,18 +18,26 @@ const bool PerTensorQuant = true;
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(32, 128/sizeof(A0DataType), 1, 4, V1, true, 0);
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64, 128/sizeof(A0DataType), 1, 4, V1, true, 0);
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V1, true, 0);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64, 64/sizeof(A0DataType), 2, 2, V1, true, 0);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 64/sizeof(A0DataType), 2, 2, V1, true, 0);
 
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(32, 128/sizeof(A0DataType), 1, 4, V1, false, 0);
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64, 128/sizeof(A0DataType), 1, 4, V1, false, 0);
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V1, false, 0);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64, 64/sizeof(A0DataType), 2, 2, V1, false, 0);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 64/sizeof(A0DataType), 2, 2, V1, false, 0);
 
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(32, 128/sizeof(A0DataType), 1, 4, V1, true, 1);
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64, 128/sizeof(A0DataType), 1, 4, V1, true, 1);
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V1, true, 1);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64, 64/sizeof(A0DataType), 2, 2, V1, true, 1);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 64/sizeof(A0DataType), 2, 2, V1, true, 1);
 
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(32, 128/sizeof(A0DataType), 1, 4, V1, false, 1);
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64, 128/sizeof(A0DataType), 1, 4, V1, false, 1);
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V1, false, 1);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64, 64/sizeof(A0DataType), 2, 2, V1, false, 1);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 64/sizeof(A0DataType), 2, 2, V1, false, 1);
 
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(32, 128/sizeof(A0DataType), 1, 4, V3, true, 0);
 
@@ -55,6 +63,13 @@ CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V3, true, 1);
 
 CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V3, false, 1);
 
-
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64,  64/sizeof(A0DataType), 2, 2, V3, true, 0);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64,  64/sizeof(A0DataType), 2, 2, V3, false, 0);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64,  64/sizeof(A0DataType), 2, 2, V3, true, 1);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(64,  64/sizeof(A0DataType), 2, 2, V3, false, 1);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 64/sizeof(A0DataType), 2, 2, V3, true, 0);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 64/sizeof(A0DataType), 2, 2, V3, false, 0);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 64/sizeof(A0DataType), 2, 2, V3, true, 1);
+CK_MOE_STAGE1_GEMM_MXFP4_DEFINE(128, 64/sizeof(A0DataType), 2, 2, V3, false, 1);
 
 
