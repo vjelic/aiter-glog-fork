d8a11834c50d0d69303a507f0df79ce36b9e61da
Tianxing Wu <tianxing.wu@amd.com>
2025-05-28 08:38:28 +0000

    Enabled xcd remapping and tuned gemm afp4wfp4

--- aiter/ops/triton/gemm_afp4wfp4.py
+++ aiter/ops/triton/gemm_afp4wfp4.py
@@ -83,7 +83,7 @@ def _gemm_afp4_wfp4_kernel(
     num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
 
     if NUM_KSPLIT == 1:
-        remap_xcd(pid, GRID_MN)
+        pid = remap_xcd(pid, GRID_MN)
 
         pid_m, pid_n = pid_grid(pid, num_pid_m, num_pid_n, GROUP_SIZE_M=GROUP_SIZE_M)
     else:
@@ -234,7 +234,7 @@ def _gemm_afp4_wfp4_kernel_preshuffled_scales(
     num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
 
     if NUM_KSPLIT == 1:
-        remap_xcd(pid, GRID_MN)
+        pid = remap_xcd(pid, GRID_MN)
 
         pid_m, pid_n = pid_grid(pid, num_pid_m, num_pid_n, GROUP_SIZE_M=GROUP_SIZE_M)
     else:
