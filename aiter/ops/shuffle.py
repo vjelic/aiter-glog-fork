113a3ebe9ec2cb36c164b0393166945478ffd726
jpvillam <jpvillam@amd.com>
2025-05-30 10:52:49 +0000

    ss

--- aiter/ops/shuffle.py
+++ aiter/ops/shuffle.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2024, Advanced Micro Devices, Inc. All rights reserved.
 
 import torch
+from aiter.utility import dtypes
 
 
 def shuffle_weight(x: torch.Tensor, layout=(16, 16), use_int4=False) -> torch.Tensor:
@@ -19,3 +20,19 @@ def shuffle_weight(x: torch.Tensor, layout=(16, 16), use_int4=False) -> torch.Te
     x_ = x_.contiguous()
     x_ = x_.view(*x.shape)
     return x_
+
+def shuffle_scale(scale: torch.Tensor) -> torch.Tensor:
+    m, n = scale.shape
+    block_size = 32
+    n = n * block_size
+    scale_padded = torch.empty(
+        (m + 255) // 256 * 256, (n // block_size + 7) // 8 * 8, dtype=torch.uint8, device=scale.device
+    ).fill_(0x7F)
+
+    scale_padded[:m, : n // block_size] = scale
+    scale = scale_padded
+    sm, sn = scale.shape
+    scale = scale.view(sm // 32, 2, 16, sn // 8, 2, 4)
+    scale = scale.permute(0, 3, 5, 2, 4, 1).contiguous()
+    scale = scale.view(sm, sn)
+    return scale.view(dtypes.fp8_e8m0)
