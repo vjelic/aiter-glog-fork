43f5efc4c97eff1c34b66e41148e0748b2fc86c4
chenjun <junchen2@amd.com>
2025-06-04 15:31:50 +0000

    update test

--- op_tests/test_gemm_a8w8_bpreshuffle.py
+++ op_tests/test_gemm_a8w8_bpreshuffle.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
 
 from aiter.test_common import checkAllclose, perftest, tensor_dump, benchmark
 import torch
@@ -8,30 +8,19 @@ import numpy as np
 import sys
 import os
 import aiter
+from aiter.utility import dtypes
+from aiter.ops.shuffle  import shuffle_weight
+import pandas as pd
 from einops import rearrange
 from einops import repeat as eirp
 
-block_shape = (128, 128)
 
 @perftest(num_iters=5)
-def run_torch2(x, weight, x_scale, w_scale, dtype=torch.float16):
-    block_shape_n, block_shape_k = block_shape
-    m, k = x.shape
-    n = weight.shape[0]
-
-    x_scale_ = eirp(x_scale, "m k -> m (k repeat)", repeat=block_shape_k)
-    x_scale_ = x_scale_[:m, :k]
-
-    w_scale_ = eirp(w_scale, "n k -> (n repeat) k", repeat=block_shape_n)
-    w_scale_ = eirp(w_scale_, "n k -> n (k repeat)", repeat=block_shape_k)
-    w_scale_ = w_scale_[:n, :k]
-
-    x_ = x.to(x_scale.dtype) * x_scale_
-    weight_ = weight.to(w_scale.dtype) * w_scale_
-
-    out = F.linear(x_.to(torch.float32), weight_.to(torch.float32))
+def run_torch(x, weight, x_scale, w_scale,  dtype=torch.bfloat16):
+    x = x.to(dtypes.fp32) * x_scale
+    weight = weight.to(dtypes.fp32) * w_scale
+    out = F.linear(x, weight)
     return out.to(dtype)
-
 @perftest()
 def run_gemm_ck_bpreshuffle(x, weight, x_scale, w_scale, dtype=torch.float16):
     return aiter.gemm_a8w8_bpreshuffle_CK(x, weight, x_scale, w_scale, dtype)
@@ -39,36 +28,30 @@ def run_gemm_ck_bpreshuffle(x, weight, x_scale, w_scale, dtype=torch.float16):
 @benchmark()
 def test_flatmm_ck(dtype, m, n, k):
     dim = (m, n, k)
-    block_shape_n, block_shape_k = block_shape
-    scale_m = m
-    scale_n = (n + block_shape_n - 1) // block_shape_n
-    scale_k = (k + block_shape_k - 1) // block_shape_k
+    x = torch.randn((m, k), dtype=dtypes.fp16, device="cuda")
+    weight = torch.randn((n, k), dtype=dtypes.fp16, device="cuda")
+    x, x_scale = aiter.pertoken_quant(x, quant_dtype=dtypes.fp8)
+    weight, w_scale = aiter.pertoken_quant(weight, quant_dtype=dtypes.fp8)
+    weight_shuffle = shuffle_weight(weight, layout=(16, 16))
 
-    x = (torch.rand((m, k), dtype=torch.float32, device="cuda") / 10).to(
-        torch.float8_e4m3fnuz
-    )
-    weight = (torch.rand((n, k), dtype=torch.float32, device="cuda") / 10).to(
-        torch.float8_e4m3fnuz
-    )
-
-    x_scale = torch.ones([scale_k, scale_m], dtype=torch.float32, device="cuda")
-    w_scale = torch.ones([scale_k, scale_n], dtype=torch.float32, device="cuda")
-
-    x_scale_trans = torch.transpose(x_scale, 0, 1)
-    w_scale_trans = torch.transpose(w_scale, 0, 1)
+    out = torch.empty(m, n, dtype=dtypes.fp16, device="cuda")
 
     flat_weight = weight.view(n // 16, 16, k // 64, 4, 16)
     flat_weight = flat_weight.permute(0, 2, 3, 1, 4).contiguous()
     flat_weight = flat_weight.view(n, -1)
-    a, avg_a = run_torch2(x, weight, x_scale_trans, w_scale_trans, dtype)
-    b, avg_b = run_gemm_ck_bpreshuffle(x, flat_weight, x_scale, w_scale, dtype)
+    a, avg_a = run_torch(x, weight, x_scale, w_scale, dtype)
+    b, avg_b = run_gemm_ck_bpreshuffle(x, weight_shuffle, x_scale, w_scale, dtype)
     tflops = 2 * m *n *k /avg_b /1e6
     msg = f"[perf] tflops: {tflops} ,dim: {str(dim):<20} dtype: {dtype}, torch avg: {avg_a:<8.2f} us,ck wpreshuffle avg: {avg_b:<8.2f} us,uplift: {avg_a/avg_b -1:<5.1%}"
     checkAllclose(a, b, msg="ck_preshuffle: " + msg, rtol=1e-2, atol=0.01)
+    return {"ck_preshuffle": avg_b}
 
+df = []
 for dtype in [torch.float16]:
-    for m in [2048]:
-        for (n, k) in [(4096, 5120),]:
-            test_flatmm_ck(dtype, m, n, k)
-            break
+    for (n, k) in [(9216, 4096),(4608, 4096),]:
+        for m in [1, 2, 4, 8, 16, 32, 64, 128, 256, 1024, 2048, 4096, 16384, 32768]:
+            ret = test_flatmm_ck(dtype, m, n, k)
+            df.append(ret)
+df = pd.DataFrame(df)
+aiter.logger.info(f"summary:\n{df}")
 
