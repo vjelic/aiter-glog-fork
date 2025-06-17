9f7089b07612b159f515a00b35a71ef89cd17b85
Juuso Korhonen <40278371+juuso-oskari@users.noreply.github.com>
2025-06-17 11:01:56 +0000

    fix chunked remap

--- aiter/ops/triton/utils/pid_preprocessing.py
+++ aiter/ops/triton/utils/pid_preprocessing.py
@@ -8,27 +8,17 @@ import triton.language as tl
 def remap_xcd_chunked(
     pid, GRID_MN, NUM_XCDS: tl.constexpr = 8, CHUNK_SIZE: tl.constexpr = 2
 ):
-    # Number of PIDs per XCD
-    pids_per_xcd = (GRID_MN + NUM_XCDS - 1) // NUM_XCDS
-    # Calculate number of tall XCDs
-    tall_xcds = GRID_MN % NUM_XCDS
-    tall_xcds = NUM_XCDS if tall_xcds == 0 else tall_xcds
     # Compute current XCD and local PID
     xcd = pid % NUM_XCDS
+    # distribute the modulo pids in round robin
+    if pid > (GRID_MN // (NUM_XCDS * CHUNK_SIZE)) * (NUM_XCDS * CHUNK_SIZE):
+        return pid
     local_pid = pid // NUM_XCDS
     # Calculate chunk index and position within chunk
     chunk_idx = local_pid // CHUNK_SIZE
     pos_in_chunk = local_pid % CHUNK_SIZE
     # Calculate new PID
-    if xcd < tall_xcds:
-        new_pid = chunk_idx * NUM_XCDS * CHUNK_SIZE + xcd * CHUNK_SIZE + pos_in_chunk
-    else:
-        new_pid = (
-            tall_xcds * pids_per_xcd * CHUNK_SIZE
-            + (xcd - tall_xcds) * (pids_per_xcd - 1) * CHUNK_SIZE
-            + chunk_idx * NUM_XCDS * CHUNK_SIZE
-            + pos_in_chunk
-        )
+    new_pid = chunk_idx * NUM_XCDS * CHUNK_SIZE + xcd * CHUNK_SIZE + pos_in_chunk
     return new_pid
 
 
