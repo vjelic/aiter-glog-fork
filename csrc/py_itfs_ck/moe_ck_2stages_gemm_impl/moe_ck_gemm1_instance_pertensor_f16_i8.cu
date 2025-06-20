354bd9f7b414b9f51b99624aeac16a6a96be8a89
root <root@quanta-cyxtera-cr66-3.ctr.dcgpu>
2025-06-20 23:38:45 +0000

    moe v3 192x128 and 96x128 configs

--- csrc/py_itfs_ck/moe_ck_2stages_gemm_impl/moe_ck_gemm1_instance_pertensor_f16_i8.cu
+++ csrc/py_itfs_ck/moe_ck_2stages_gemm_impl/moe_ck_gemm1_instance_pertensor_f16_i8.cu
@@ -16,28 +16,45 @@ CK_MOE_STAGE1_GEMM_DEFINE(32, 256/sizeof(A0DataType), 1, 4, V1, true, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(32, 128/sizeof(A0DataType), 1, 4, V1, true, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(64, 256/sizeof(A0DataType), 1, 4, V1, true, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(64, 128/sizeof(A0DataType), 1, 4, V1, true, 1)
+CK_MOE_STAGE1_GEMM_DEFINE(96, 128/sizeof(A0DataType), 1, 4, V1, true, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V1, true, 1)
+CK_MOE_STAGE1_GEMM_DEFINE(192, 128/sizeof(A0DataType), 1, 4, V1, true, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(256, 128/sizeof(A0DataType), 1, 4, V1, true, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(32, 256/sizeof(A0DataType), 1, 4, V1, true, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(32, 128/sizeof(A0DataType), 1, 4, V1, true, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(64, 256/sizeof(A0DataType), 1, 4, V1, true, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(64, 128/sizeof(A0DataType), 1, 4, V1, true, 0)
+CK_MOE_STAGE1_GEMM_DEFINE(96, 128/sizeof(A0DataType), 1, 4, V1, true, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V1, true, 0)
+CK_MOE_STAGE1_GEMM_DEFINE(192, 128/sizeof(A0DataType), 1, 4, V1, true, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(256, 128/sizeof(A0DataType), 1, 4, V1, true, 0)
 
 CK_MOE_STAGE1_GEMM_DEFINE(32, 256/sizeof(A0DataType), 1, 4, V1, false, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(32, 128/sizeof(A0DataType), 1, 4, V1, false, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(64, 256/sizeof(A0DataType), 1, 4, V1, false, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(64, 128/sizeof(A0DataType), 1, 4, V1, false, 1)
+CK_MOE_STAGE1_GEMM_DEFINE(96, 128/sizeof(A0DataType), 1, 4, V1, false, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V1, false, 1)
+CK_MOE_STAGE1_GEMM_DEFINE(192, 128/sizeof(A0DataType), 1, 4, V1, false, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(256, 128/sizeof(A0DataType), 1, 4, V1, false, 1)
 CK_MOE_STAGE1_GEMM_DEFINE(32, 256/sizeof(A0DataType), 1, 4, V1, false, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(32, 128/sizeof(A0DataType), 1, 4, V1, false, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(64, 256/sizeof(A0DataType), 1, 4, V1, false, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(64, 128/sizeof(A0DataType), 1, 4, V1, false, 0)
+CK_MOE_STAGE1_GEMM_DEFINE(96, 128/sizeof(A0DataType), 1, 4, V1, false, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V1, false, 0)
+CK_MOE_STAGE1_GEMM_DEFINE(192, 128/sizeof(A0DataType), 1, 4, V1, false, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(256, 128/sizeof(A0DataType), 1, 4, V1, false, 0)
 
+
+
+CK_MOE_STAGE1_GEMM_DEFINE(96, 128/sizeof(A0DataType), 1, 4, V3, true, 1)
+
+CK_MOE_STAGE1_GEMM_DEFINE(96, 128/sizeof(A0DataType), 1, 4, V3, true, 0)
+
+CK_MOE_STAGE1_GEMM_DEFINE(96, 128/sizeof(A0DataType), 1, 4, V3, false, 1)
+
+CK_MOE_STAGE1_GEMM_DEFINE(96, 128/sizeof(A0DataType), 1, 4, V3, false, 0)
 
 
 CK_MOE_STAGE1_GEMM_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V3, true, 1)
@@ -47,6 +64,16 @@ CK_MOE_STAGE1_GEMM_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V3, true, 0)
 CK_MOE_STAGE1_GEMM_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V3, false, 1)
 
 CK_MOE_STAGE1_GEMM_DEFINE(128, 128/sizeof(A0DataType), 1, 4, V3, false, 0)
+
+
+CK_MOE_STAGE1_GEMM_DEFINE(192, 128/sizeof(A0DataType), 1, 4, V3, true, 1)
+
+CK_MOE_STAGE1_GEMM_DEFINE(192, 128/sizeof(A0DataType), 1, 4, V3, true, 0)
+
+CK_MOE_STAGE1_GEMM_DEFINE(192, 128/sizeof(A0DataType), 1, 4, V3, false, 1)
+
+CK_MOE_STAGE1_GEMM_DEFINE(192, 128/sizeof(A0DataType), 1, 4, V3, false, 0)
+
 
 CK_MOE_STAGE1_GEMM_DEFINE(256, 128/sizeof(A0DataType), 1, 4, V3, true, 1)
 
