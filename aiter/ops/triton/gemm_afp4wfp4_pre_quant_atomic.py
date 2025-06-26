50f1da65e940dcbd27203f651ad300973f62dba1
ShaoChunLee <Shao-Chun.Lee@amd.com>
2025-06-26 21:45:55 +0000

    tune fp4 prequant gemm atomic

--- aiter/ops/triton/gemm_afp4wfp4_pre_quant_atomic.py
+++ aiter/ops/triton/gemm_afp4wfp4_pre_quant_atomic.py
@@ -217,9 +217,21 @@ def _get_config(
                 _get_config._config_dict[key] = config
         else:
             key = "default"  # fall back to default config
-
-    # TODO enable and optimize for all configs
-    return _get_config._config_dict[key]["small"]
+    
+    if M < 32:
+        return _get_config._config_dict[key]["small"]
+    elif M <= 128:
+        BLK_M = triton.next_power_of_2(M)
+        if BLK_M == 32:
+            return _get_config._config_dict[key]["medium_M32"]
+        elif BLK_M == 64:
+            return _get_config._config_dict[key]["medium_M64"]
+        elif BLK_M == 128:
+            return _get_config._config_dict[key]["medium_M128"]
+    elif M <= 256:
+        return _get_config._config_dict[key]["large"]
+    else:
+        return _get_config._config_dict[key]["xlarge"]
 
 
 def gemm_afp4wfp4_pre_quant(
