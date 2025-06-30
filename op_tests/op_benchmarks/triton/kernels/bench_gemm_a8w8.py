a4b0e5099737cdbafbd87432d570331694a8470c
William Zhou <William.Zhou@amd.com>
2025-06-30 15:18:55 +0000

    Update table formatting for bench_gemm_a8w8 and add tests for bench_gemm_a8w8 benchmarking.

--- op_tests/op_benchmarks/triton/kernels/bench_gemm_a8w8.py
+++ op_tests/op_benchmarks/triton/kernels/bench_gemm_a8w8.py
@@ -1,4 +1,3 @@
-import argparse
 import sys
 import triton
 from aiter.ops.triton.gemm_a8w8 import gemm_a8w8
@@ -6,11 +5,12 @@ from aiter.ops.triton.utils.types import str_to_torch_dtype
 from op_tests.triton_tests.test_gemm_a8w8 import (
     generate_gemm_a8w8_inputs,
 )
+from op_tests.op_benchmarks.triton.utils.argparse import get_parser, add_argparse_ff
 from op_tests.op_benchmarks.triton.utils.benchmark_utils import (
     get_model_configs,
-    get_available_models,
 )
 import warnings
+import matplotlib.pyplot as plt
 
 
 def model_benchmark_shapes(args):
@@ -20,16 +20,17 @@ def model_benchmark_shapes(args):
     shapes = []
     for M in M_list:
         for _, config in configs.items():
-            # op order: (M, N, K)
-            if args.fc1:
-                shapes.append((M, config["hidden_size"], config["intermediate_size"]))
-            if args.fc2:
-                shapes.append((M, config["intermediate_size"], config["hidden_size"]))
+            N = config["hidden_size"]
+            K = config["intermediate_size"]
+            shapes.append((M, N, K))
 
     return shapes
 
 
 def get_x_vals():
+    """
+    Get a default set of benchmarking values (M, N, K).
+    """
     x_vals = [
         (1, 1280, 8192),
         (32, 1280, 8192),
@@ -48,22 +49,94 @@ def get_x_vals():
     return x_vals
 
 
-def run_benchmark(args):
-    assert not (args.shape and args.model) or not (
-        args.shape and args.M
-    ), "User can specify --shape or --model MODEL -M VAL exclusively"
+def run_model_benchmark(args):
+    x_names = ["M", "hidden_dim", "intermediate_dim"]
+    if not args.fc1 and not args.fc2:
+        # by default, benchmark both
+        warnings.warn(
+            "No specific layer selected for benchmarking, defaulting to both. To specify a layer, use -fc1 or -fc2."
+        )
+        args.fc1 = True
+        args.fc2 = True
+    x_vals_list = model_benchmark_shapes(args)
 
+    if args.metric == "time":
+        ylabel = "Time (ms)"
+    elif args.metric == "throughput":
+        ylabel = "Throughput (TFLOPS)"
+    elif args.metric == "bandwidth":
+        ylabel = "Bandwidth (GB/s)"
+    else:
+        raise NotImplementedError(f"{args.metric} is not supported")
+
+    line_names = []
+    if args.fc1:
+        line_names.append("fc1")
+    if args.fc2:
+        line_names.append("fc2")
+    line_vals = line_names
+
+    mpl_colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+    benchmark = triton.testing.Benchmark(
+        x_names=x_names,
+        x_vals=x_vals_list,
+        line_arg="layer",
+        line_vals=line_vals,
+        line_names=line_names,
+        styles=[
+            (mpl_colors[i], "-") for i in range(len(line_names))
+        ],  # match line names to colors
+        ylabel=ylabel,
+        plot_name="GEMM A8W8 Benchmark",
+        args={"metric": args.metric},
+    )
+
+    @triton.testing.perf_report([benchmark])
+    def bench_gemm_a8w8(M, hidden_dim, intermediate_dim, metric, layer, **kwargs):
+        # NOTE: Assume bias and output has the same dtype
+        c_dtype = str_to_torch_dtype["bf16"]
+
+        if layer == "fc1":
+            N, K = hidden_dim, intermediate_dim
+        else:
+            N, K = intermediate_dim, hidden_dim
+
+        x, weight, x_scale, w_scale, bias, y = generate_gemm_a8w8_inputs(
+            M, N, K, str_to_torch_dtype["fp8e4m3"], c_dtype, output=True
+        )
+        # flops
+        flops = 2.0 * M * N * K
+        # memory transfer
+        mem_read = (M * K) * x.element_size() + (N * K) * weight.element_size()
+        mem_write = (M * N) * bias.element_size()
+        mem = mem_read + mem_write
+
+        ms = triton.testing.do_bench(
+            lambda: gemm_a8w8(
+                x, weight, x_scale, w_scale, bias, c_dtype, y
+            ),  # noqa: E731
+            warmup=25,
+            rep=100,
+        )
+
+        # Return exactly one scalar depending on which metric is active
+        if metric == "time":
+            return ms
+        elif metric == "throughput":
+            tflops = flops / ms * 1e-9
+            return tflops
+        elif metric == "bandwidth":
+            bandwidth = mem / (ms * 1e-3) * 1e-9  # GB/s
+            return bandwidth
+        else:
+            raise ValueError("Unknown metric: " + metric)
+
+    bench_gemm_a8w8.run(save_path=".", print_data=True)
+
+
+def run_size_benchmark(args):
     x_names = ["M", "N", "K"]
-    if args.model:
-        if not args.fc1 and not args.fc2:
-            # by default, benchmark both
-            warnings.info(
-                "No specific layer selected for benchmarking, defaulting to both. To specify a layer, use -fc1 or -fc2."
-            )
-            args.fc1 = True
-            args.fc2 = True
-        x_vals_list = model_benchmark_shapes(args)
-    elif args.shape:
+    if args.shape:
         x_vals_list = [args.shape]
     else:
         x_vals_list = get_x_vals()
@@ -77,14 +150,12 @@ def run_benchmark(args):
     else:
         raise NotImplementedError(f"{args.metric} is not supported")
 
-    line_names = ["Triton"]
-    line_vals = ["triton"]
     benchmark = triton.testing.Benchmark(
         x_names=x_names,
         x_vals=x_vals_list,
-        line_arg="provider",
-        line_vals=line_vals,
-        line_names=line_names,
+        line_arg="Provider",
+        line_vals=["Triton"],
+        line_names=["Triton"],
         styles=[("green", "-")],
         ylabel=ylabel,
         plot_name="GEMM A8W8 Benchmark",
@@ -92,7 +163,7 @@ def run_benchmark(args):
     )
 
     @triton.testing.perf_report([benchmark])
-    def bench_gemm_a8w8(M, N, K, metric, provider):
+    def bench_gemm_a8w8(M, N, K, metric, **kwargs):
         # NOTE: Assume bias and output has the same dtype
         c_dtype = str_to_torch_dtype["bf16"]
         x, weight, x_scale, w_scale, bias, y = generate_gemm_a8w8_inputs(
@@ -128,55 +199,19 @@ def run_benchmark(args):
     bench_gemm_a8w8.run(save_path=".", print_data=True)
 
 
+def run_benchmark(args):
+    assert not (args.shape and args.model) or not (
+        args.shape and args.M
+    ), "User can specify --shape or --model MODEL -M VAL exclusively"
+    if args.model:
+        run_model_benchmark(args)
+    else:
+        run_size_benchmark(args)
+
+
 def parse_args():
-    parser = argparse.ArgumentParser(
-        prog="Benchmark A8W8 GEMM",
-        allow_abbrev=False,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    available_models = get_available_models()  # Dynamically load model names
-    model_help = (
-        "Model name to benchmark. Select from: ["
-        + ", ".join(available_models)
-        + "]. Use 'all' to benchmark all models or leave blank for the default benchmark script."
-    )
-    parser.add_argument(
-        "--model-configs",
-        type=str,
-        default="utils/model_configs.json",
-        help="Model config json file.",
-    )
-    parser.add_argument("--model", type=str, help=model_help)
-    parser.add_argument(
-        "-M",
-        type=int,
-        default=4096,
-        help="M dim of model benchmark if only one model is under test",
-    )
-    parser.add_argument(
-        "-fc1",
-        action="store_true",
-        help="Benchmark the up-projection (hidden dim to intermediate dim in the feed-forward layer)",
-    )
-    parser.add_argument(
-        "-fc2",
-        action="store_true",
-        help="Benchmark the down-projection (intermediate dim to hidden dim in the feed-forward layer)",
-    )
-    parser.add_argument(
-        "--shape",
-        type=int,
-        nargs=3,
-        metavar=("M", "N", "K"),
-        help="user-defined shape to benchmark",
-    )
-    parser.add_argument(
-        "--metric",
-        type=str,
-        choices=["time", "throughput", "bandwidth"],
-        default="throughput",
-        help="metric to plot",
-    )
+    parser = get_parser(kernel_name="A8W8 GEMM")
+    parser = add_argparse_ff(parser)
     args = parser.parse_args()
     return args
 
