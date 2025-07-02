9bf46a7968e6943117d703abe35fe298f90cb9b0
chenjun <46212055+junhaha666@users.noreply.github.com>
2025-07-02 14:34:05 +0800

    Dispatch combine (#571)
    * add dispatch combine
    
    * fix dispatch combine bug
    
    * add moe ep to test
    
    * format
    
    * ci
    
    * remove ck_moe
    
    * fix  prebuild
    
    * update ck
    
    * update test for mori update
    
    * update
    
    * clear code
    
    * update test
    
    * add quant argument for test
    
    ---------
    
    Co-authored-by: valarLip <340077269@qq.com>
    Co-authored-by: TianDi101 <Di.Tian2@amd.com>
    Co-authored-by: amd-ruitang3 <Rui.Tang2@amd.com>

--- csrc/include/moe_ck.h
+++ csrc/include/moe_ck.h
@@ -4,19 +4,6 @@
 
 #include <torch/extension.h>
 
-torch::Tensor
-ck_moe(torch::Tensor& hidden_states,          // [m, k], input token
-       torch::Tensor& w1,                     // [e, n, k]/[e, 2*n, k], pre-shuffle([e, nr, kr, w])
-       torch::Tensor& w2,                     // [e, n, k], pre-shuffle([e, nr, kr, w])
-       torch::Tensor& topk_weights,           // [tokens, topk]
-       torch::Tensor& topk_ids,               // [tokens, topk]
-       std::optional<torch::Tensor> w1_scale, // [e, 1, n], gate(up) scale
-       std::optional<torch::Tensor> w2_scale, // [e, 1, k], down scale
-       std::optional<torch::Tensor> a1_scale, // [m, 1], token scale
-       std::optional<torch::Tensor> a2_scale, // [e, 1, n], smooth-quant-scale for 2nd gemm input
-       std::optional<int> block_m               = 32,
-       std::optional<torch::Tensor> expert_mask = std::nullopt);
-
 void ck_moe_stage1(torch::Tensor& hidden_states, // [m, k], input token
                    torch::Tensor& w1, // [e, n, k]/[e, 2*n, k], pre-shuffle([e, nr, kr, w])
                    torch::Tensor& w2, // [e, n, k], pre-shuffle([e, nr, kr, w])
