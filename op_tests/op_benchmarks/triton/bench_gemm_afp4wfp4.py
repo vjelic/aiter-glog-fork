Improve code reuse + fix benchmarking FLOP computation bug

op_tests/op_benchmarks/triton/bench_gemm_afp4wfp4.py
@@ -8,7 +8,11 @@ from aiter.ops.triton.gemm_afp4wfp4 import (
     gemm_afp4wfp4_preshuffled_scales,
 )
 from op_tests.triton_tests.test_gemm_afp4wfp4 import generate_gemm_afp4wfp4_inputs
-from op_tests.op_benchmarks.triton.utils.argparse import get_parser, add_argparse_ff
+from op_tests.op_benchmarks.triton.utils.argparse import (
+    get_parser, 
+    add_argparse_ff,
+    get_ff_args,
+)
 from op_tests.op_benchmarks.triton.utils.benchmark_utils import (
     get_model_benchmark_object,
     get_shape_benchmark_object,
@@ -19,25 +23,49 @@ TRITON_HIP_PRESHUFFLE_SCALES = (
     os.environ.get("TRITON_HIP_PRESHUFFLE_SCALES", "0") == "1"
 )
 
+def bench_gemm_fn(M, N, K, metric):
+    c_dtype = torch.bfloat16
+    x, w, _, _, x_scale, w_scale, _, _ = generate_gemm_afp4wfp4_inputs(
+        M, N, K, c_dtype
+    )
+    # flops
+    flops = 2.0 * M * N * K
+    # memory transfer
+    mem_read = x.numel() * x.element_size() + w.numel() * w.element_size()
+    mem_read += (
+        x_scale.numel() * x_scale.element_size()
+        + w_scale.numel() * w_scale.element_size()
+    )
+    mem_write = (M * N) * 2  # TODO: Fix for c_dtype != bf16
+    mem = mem_read + mem_write
+    out = torch.empty(x.shape[0], w.shape[1], device=x.device, dtype=c_dtype)
 
-def get_x_vals():
-    x_vals = [
-        (1, 1280, 8192),
-        (32, 1280, 8192),
-        (64, 1280, 8192),
-        (128, 1280, 8192),
-        (192, 1280, 8192),
-        (256, 1280, 8192),
-        (320, 1280, 8192),
-        (512, 1280, 8192),
-        (1024, 1280, 8192),
-        (2048, 1280, 8192),
-        (4096, 1280, 8192),
-        (8192, 1280, 8192),
-        (16384, 1280, 8192),
-    ]
-    return x_vals
+    if TRITON_HIP_PRESHUFFLE_SCALES:
+        ms = triton.testing.do_bench(
+            lambda: gemm_afp4wfp4_preshuffled_scales(
+                x, w, x_scale, w_scale, c_dtype, out
+            ),
+            warmup=25,
+            rep=100,
+        )
+    else:
+        ms = triton.testing.do_bench(
+            lambda: gemm_afp4wfp4(x, w, x_scale, w_scale, c_dtype, out),
+            warmup=25,
+            rep=100,
+        )
 
+    # Return exactly one scalar depending on which metric is active
+    if metric == "time":
+        return ms
+    elif metric == "throughput":
+        tflops = flops / ms * 1e-9
+        return tflops
+    elif metric == "bandwidth":
+        bandwidth = mem / (ms * 1e-3) * 1e-9  # GB/s
+        return bandwidth
+    else:
+        raise ValueError("Unknown metric: " + metric)
 
 def run_benchmark(args, defaults):
     assert not (args.shape and args.model) or not (
@@ -85,48 +113,8 @@ def run_model_benchmark(args):
             N, K = hidden_dim, intermediate_dim
             # Divide K by tensor parallel
             K = math.ceil(K / args.tp)
-        c_dtype = torch.bfloat16
-        x, w, _, _, x_scale, w_scale, _, _ = generate_gemm_afp4wfp4_inputs(
-            M, N, K, c_dtype
-        )
-        # flops
-        flops = 2.0 * M * N * K
-        # memory transfer
-        mem_read = x.numel() * x.element_size() + w.numel() * w.element_size()
-        mem_read += (
-            x_scale.numel() * x_scale.element_size()
-            + w_scale.numel() * w_scale.element_size()
-        )
-        mem_write = (M * N) * 2  # TODO: Fix for c_dtype != bf16
-        mem = mem_read + mem_write
-        out = torch.empty(x.shape[0], w.shape[1], device=x.device, dtype=c_dtype)
-
-        if TRITON_HIP_PRESHUFFLE_SCALES:
-            ms = triton.testing.do_bench(
-                lambda: gemm_afp4wfp4_preshuffled_scales(
-                    x, w, x_scale, w_scale, c_dtype, out
-                ),
-                warmup=25,
-                rep=100,
-            )
-        else:
-            ms = triton.testing.do_bench(
-                lambda: gemm_afp4wfp4(x, w, x_scale, w_scale, c_dtype, out),
-                warmup=25,
-                rep=100,
-            )
-
-        # Return exactly one scalar depending on which metric is active
-        if metric == "time":
-            return ms
-        elif metric == "throughput":
-            tflops = flops / ms * 1e-9
-            return tflops
-        elif metric == "bandwidth":
-            bandwidth = mem / (ms * 1e-3) * 1e-9  # GB/s
-            return bandwidth
-        else:
-            raise ValueError("Unknown metric: " + metric)
+ 
+        return bench_gemm_fn(M, N, K, metric)
 
     bench_gemm_afp4wfp4_blockscale.run(save_path=".", print_data=True)
 
@@ -136,48 +124,7 @@ def run_shape_benchmark(args):
 
     @triton.testing.perf_report([benchmark])
     def bench_gemm_afp4wfp4_blockscale(M, N, K, metric, **kwargs):
-        c_dtype = torch.bfloat16
-        x, w, _, _, x_scale, w_scale, _, _ = generate_gemm_afp4wfp4_inputs(
-            M, N, K, c_dtype
-        )
-        # flops
-        flops = 2.0 * M * N * K
-        # memory transfer
-        mem_read = x.numel() * x.element_size() + w.numel() * w.element_size()
-        mem_read += (
-            x_scale.numel() * x_scale.element_size()
-            + w_scale.numel() * w_scale.element_size()
-        )
-        mem_write = (M * N) * 2  # TODO: Fix for c_dtype != bf16
-        mem = mem_read + mem_write
-        out = torch.empty(x.shape[0], w.shape[1], device=x.device, dtype=c_dtype)
-
-        if TRITON_HIP_PRESHUFFLE_SCALES:
-            ms = triton.testing.do_bench(
-                lambda: gemm_afp4wfp4_preshuffled_scales(
-                    x, w, x_scale, w_scale, c_dtype, out
-                ),
-                warmup=25,
-                rep=100,
-            )
-        else:
-            ms = triton.testing.do_bench(
-                lambda: gemm_afp4wfp4(x, w, x_scale, w_scale, c_dtype, out),
-                warmup=25,
-                rep=100,
-            )
-
-        # Return exactly one scalar depending on which metric is active
-        if metric == "time":
-            return ms
-        elif metric == "throughput":
-            tflops = flops / ms * 1e-9
-            return tflops
-        elif metric == "bandwidth":
-            bandwidth = mem / (ms * 1e-3) * 1e-9  # GB/s
-            return bandwidth
-        else:
-            raise ValueError("Unknown metric: " + metric)
+        return bench_gemm_fn(M, N, K, metric)
 
     bench_gemm_afp4wfp4_blockscale.run(save_path=".", print_data=True)
 
@@ -191,10 +138,7 @@ def parse_args():
         action="store_true",
         help="Print VGPR usage for Triton kernels.",
     )
-    args = parser.parse_args()
-    defaults = parser.parse_args([])
-    return args, defaults
-
+    return get_ff_args(parser)
 
 def main():
     args, defaults = parse_args()