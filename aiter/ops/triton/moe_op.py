748961e651a843c6dc58861c33b4ae3c35a7b81b
Juuso Korhonen <40278371+juuso-oskari@users.noreply.github.com>
2025-07-16 06:20:12 +0000

    fix

--- aiter/ops/triton/moe_op.py
+++ aiter/ops/triton/moe_op.py
@@ -845,6 +845,8 @@ def _fused_moe_persistent_kernel(
         offs_token = tl.load(sorted_token_ids_ptr + offs_token_id)
         token_mask = offs_token < num_valid_tokens
 
+
+
         off_experts = tl.load(expert_ids_ptr + pid_m).to(tl.int64)
         if off_experts == -1:
             # -----------------------------------------------------------
@@ -868,7 +870,9 @@ def _fused_moe_persistent_kernel(
                 offs_token[:, None] // top_k * stride_am + offs_k[None, :] * stride_ak
             )
             # Compute the B pointer
-            offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+            offs_bn = (
+                pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N).to(tl.int64)
+            ) % N
             b_ptrs = (
                 b_ptr
                 + off_experts * stride_be
