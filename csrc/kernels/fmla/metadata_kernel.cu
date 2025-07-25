3c9d5c0d963a8db5cd88a6733ad9bd01b6fc0ccb
zanzhang <zanzhang@amd.com>
2025-07-25 08:15:00 +0800

    for mi300x

--- csrc/kernels/fmla/metadata_kernel.cu
+++ csrc/kernels/fmla/metadata_kernel.cu
@@ -197,7 +197,9 @@ std::vector<torch::Tensor> get_mla_metadata_impl(
 
     const int32_t batch_size = kv_indptr.size(0) - 1;
     const int32_t cu_num =
-        ROUND(batch_size, 16) * dev_prop.multiProcessorCount;
+        // ROUND(batch_size, 16) * dev_prop.multiProcessorCount;
+        // !!!!!!!!JUST FOR MI300!!!!!!!!!!!!!!
+        ROUND(batch_size, 16) * 80;
     auto opt = kv_indptr.options();
 
     // declare outputs
