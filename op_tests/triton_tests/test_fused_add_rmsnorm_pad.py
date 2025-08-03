2f40fc04f05e2f836bd0ef76e80a059e3c494ca4
ShaoChunLee <Shao-Chun.Lee@amd.com>
2025-08-03 21:54:59 +0000

    add test

---
+++ op_tests/triton_tests/test_fused_add_rmsnorm_pad.py
@@ -0,0 +1,50 @@
+import torch
+import pytest
+from aiter.ops.triton.fused_add_rmsnorm_pad import fused_add_rmsnorm_pad
+import torch.nn.functional as F
+
+def generate_inputs(M, N, has_res, dtype):
+    x = torch.randn((M, N), dtype=dtype, device="cuda")
+    weight = torch.randn((N, ), dtype=dtype, device="cuda")
+    res = torch.randn((M, N), dtype=dtype, device="cuda") if has_res else None
+    return x, weight, res
+
+
+def run_torch(x, weight, eps = 1e-6, res = None, pad_to_multiple = 0):
+    dtype = x.dtype
+    x = x.to(torch.float32)
+    if res is not None:
+        x = x + res.to(torch.float32)
+        res = x.to(dtype)
+    variance = x.pow(2).mean(dim=-1, keepdim=True)
+    x = x * weight * torch.rsqrt(variance + eps)
+    N = x.shape[-1]
+    if pad_to_multiple > 0:
+        pad = (N + pad_to_multiple - 1) // pad_to_multiple * pad_to_multiple - N
+        if pad > 0:
+            x = F.pad(x, (0, pad, 0, 0), "constant", 0.0)
+    x = x.to(dtype)
+    if res is not None:
+        return x, res
+    return x
+
+
+@pytest.mark.parametrize("M", [1, 4, 8, 16, 32, 256, 8192])
+@pytest.mark.parametrize("N", [4, 16, 320, 640, 2880])
+@pytest.mark.parametrize("has_res", [False, True])
+@pytest.mark.parametrize("pad_to_multiple", [0, 256])
+@pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16, torch.float32])
+def test_mul_add(M: int, N: int, has_res: bool, pad_to_multiple: int, dtype):
+
+    x, weight, res = generate_inputs(M, N, has_res, dtype)
+
+    if has_res:
+        x_torch, res_torch = run_torch(x, weight, 1e-6, res, pad_to_multiple)
+        x_triton, res_triton = fused_add_rmsnorm_pad(x, weight, 1e-6, res, pad_to_multiple)
+    else:
+        x_torch = run_torch(x, weight, 1e-6, res, pad_to_multiple)
+        x_triton = fused_add_rmsnorm_pad(x, weight, 1e-6, res, pad_to_multiple)
+
+    torch.testing.assert_close(x_torch.to(dtype), x_triton)
+    if has_res:
+        torch.testing.assert_close(res_torch.to(dtype), res_triton)
---
+++ op_tests/triton_tests/test_fused_qkv_split_qk_rope.py
@@ -0,0 +1,80 @@
+import torch
+import pytest
+from aiter.ops.triton.fused_qkv_split_qk_rope import fused_qkv_split_qk_rope
+from op_tests.triton_tests.test_fused_qk_concat import generate_rope_cached_freqs
+from op_tests.test_rope import ref_rope_sbhd_fwd, RotateStyle
+
+
+def generate_qkv_inputs(B: int, QH_PER_KH: int, KH: int, D: int, nope: bool, nope_first: bool, dtype):
+    qkv = torch.randn((B, (QH_PER_KH * KH + 2 * KH) * (D * (2 if nope else 1))), dtype=dtype, device="cuda")
+    return qkv
+
+def run_torch( 
+    qkv, QH_PER_KH, KH, D, ref_freqs, reuse_freqs_front_part, nope, nope_first, rotate_style
+):
+    q_size = QH_PER_KH * KH * D
+    kv_size = KH * D
+    q, k, v = qkv.split([q_size, kv_size, kv_size], dim=-1)
+    q = q.view(-1, QH_PER_KH * KH, D).contiguous()
+    k = k.view(-1, KH, D).contiguous()
+    v = v.view(-1, KH, D).contiguous()
+
+    q = ref_rope_sbhd_fwd(
+        q,
+        ref_freqs,
+        rotate_style=rotate_style,
+        reuse_freqs_front_part=reuse_freqs_front_part,
+        nope_first=nope_first,
+    )
+    k = ref_rope_sbhd_fwd(
+        k,
+        ref_freqs,
+        rotate_style=rotate_style,
+        reuse_freqs_front_part=reuse_freqs_front_part,
+        nope_first=nope_first,
+    )
+
+    return q, k, v
+
+
+# @pytest.mark.parametrize("B", [32])
+# @pytest.mark.parametrize("QH_PER_KH", [8])
+# @pytest.mark.parametrize("KH", [8])
+# @pytest.mark.parametrize("D", [64])
+@pytest.mark.parametrize("B", [1, 4, 8, 16, 32])
+@pytest.mark.parametrize("QH_PER_KH", [1, 2, 4, 8, 16])
+@pytest.mark.parametrize("KH", [1, 4])
+@pytest.mark.parametrize("D", [64, 128])
+@pytest.mark.parametrize("rotate_style", [RotateStyle.GPTJ, RotateStyle.NEOX])
+@pytest.mark.parametrize("max_embed_positions", [131072])
+@pytest.mark.parametrize(
+    "nope, nope_first", [(False, False), (True, False), (True, True)]
+)
+@pytest.mark.parametrize("reuse_freqs_front_part", [False, True])
+@pytest.mark.parametrize("dtype", [torch.bfloat16])
+def test_fused_qkv_split_qk_rope(
+    B: int,
+    QH_PER_KH: int,
+    KH: int,
+    D: int,
+    rotate_style: int,
+    max_embed_positions: int,
+    nope: bool,
+    nope_first: bool,
+    reuse_freqs_front_part: bool,
+    dtype: torch.dtype,
+):
+
+    qkv = generate_qkv_inputs(B, QH_PER_KH, KH, D, nope, nope_first, dtype)
+    
+    pos, freqs, cos, sin = generate_rope_cached_freqs(
+        B, max_embed_positions, (D // 2) if reuse_freqs_front_part else D, dtype
+    )
+    ref_freqs = freqs[pos].squeeze(-2)
+    
+    q_triton, k_triton, v_triton = fused_qkv_split_qk_rope(qkv, cos, sin, pos, QH_PER_KH*KH, KH, (D * (2 if nope else 1)), is_neox=(rotate_style == RotateStyle.NEOX), offsets=None, reuse_freqs_front_part=reuse_freqs_front_part, nope_first=nope_first)
+    q_torch, k_torch, v_torch = run_torch(qkv, QH_PER_KH, KH, (D * (2 if nope else 1)), ref_freqs, reuse_freqs_front_part, nope, nope_first, rotate_style)
+
+    torch.testing.assert_close(q_torch, q_triton)
+    torch.testing.assert_close(k_torch, k_triton)
+    torch.testing.assert_close(v_torch, v_triton)
