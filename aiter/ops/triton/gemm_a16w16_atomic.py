4cf522b6abf9338817aa06cd292574f93424d94a
Lukasz Burzawa <lukasz.burzawa@amd.com>
2025-08-07 22:29:06 +0000

    fix atomic gemm implementation for uneven K

--- aiter/ops/triton/gemm_a16w16_atomic.py
+++ aiter/ops/triton/gemm_a16w16_atomic.py
@@ -18,9 +18,7 @@ _LOGGER = AiterTritonLogger()
 
 @triton.heuristics(
     {
-        "EVEN_K": lambda args: (args["K"] % (args["BLOCK_SIZE_K"]) == 0)
-        and (args["SPLITK_BLOCK_SIZE"] % args["BLOCK_SIZE_K"] == 0)
-        and (args["K"] % (args["SPLITK_BLOCK_SIZE"]) == 0),
+        "EVEN_K": lambda args: (args["K"] % (args["BLOCK_SIZE_K"]) == 0),
         "GRID_MN": lambda args: triton.cdiv(args["M"], args["BLOCK_SIZE_M"])
         * triton.cdiv(args["N"], args["BLOCK_SIZE_N"]),
     }
@@ -45,7 +43,7 @@ def _gemm_a16_w16_atomic_kernel(
     BLOCK_SIZE_K: tl.constexpr,
     GROUP_SIZE_M: tl.constexpr,
     NUM_KSPLIT: tl.constexpr,
-    SPLITK_BLOCK_SIZE: tl.constexpr,
+    DISABLE_BUFFER_LOADS: tl.constexpr,
     cache_modifier: tl.constexpr,
     EVEN_K: tl.constexpr,
     GRID_MN: tl.constexpr,
@@ -54,12 +52,13 @@ def _gemm_a16_w16_atomic_kernel(
     A has shape (M, K), B has shape (K, N) and C has shape (M, N)
     """
 
-    tl.assume(stride_am > 0)
-    tl.assume(stride_ak > 0)
-    tl.assume(stride_bk > 0)
-    tl.assume(stride_bn > 0)
-    tl.assume(stride_cm > 0)
-    tl.assume(stride_cn > 0)
+    if not DISABLE_BUFFER_LOADS:
+        tl.assume(stride_am > 0)
+        tl.assume(stride_ak > 0)
+        tl.assume(stride_bk > 0)
+        tl.assume(stride_bn > 0)
+        tl.assume(stride_cm > 0)
+        tl.assume(stride_cn > 0)
 
     # -----------------------------------------------------------
     # Map program ids `pid` to the block of C it should compute.
@@ -80,55 +79,54 @@ def _gemm_a16_w16_atomic_kernel(
     tl.assume(pid_m >= 0)
     tl.assume(pid_n >= 0)
     tl.assume(pid_k >= 0)
-    if (pid_k * SPLITK_BLOCK_SIZE) < K:
-        num_k_iter = tl.cdiv(SPLITK_BLOCK_SIZE, BLOCK_SIZE_K)
 
-        # Create pointers for first block of A and B input matrices
-        offs_k = tl.arange(0, BLOCK_SIZE_K)
-        offs_k_split = pid_k * (SPLITK_BLOCK_SIZE) + offs_k
-        offs_am = (pid_m.to(tl.int64) * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
-        offs_bn = (pid_n.to(tl.int64) * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
-        a_ptrs = a_ptr + (
-            offs_am[:, None] * stride_am + offs_k_split[None, :] * stride_ak
-        )
-        b_ptrs = b_ptr + (
-            offs_k_split[:, None] * stride_bk + offs_bn[None, :] * stride_bn
-        )
+    num_k_iter = tl.cdiv(K, NUM_KSPLIT * BLOCK_SIZE_K)
 
-        acc_dtype = tl.float32 if c_ptr.type.element_ty != tl.int8 else tl.int32
-        accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=acc_dtype)
+    # Create pointers for first block of A and B input matrices
+    offs_k = pid_k * BLOCK_SIZE_K + tl.arange(0, BLOCK_SIZE_K)
+    offs_am = (pid_m.to(tl.int64) * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+    offs_bn = (pid_n.to(tl.int64) * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+    a_ptrs = a_ptr + (
+        offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak
+    ).to(tl.int64)
+    b_ptrs = b_ptr + (
+        offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn
+    ).to(tl.int64)
 
-        for k in range(pid_k * num_k_iter, (pid_k + 1) * num_k_iter):
-            # Load the next block of A and B, generate a mask by checking the K dimension.
-            # If it is out of bounds, set it to 0.
-            if EVEN_K:
-                a = tl.load(a_ptrs)
-                b = tl.load(b_ptrs, cache_modifier=cache_modifier)
-            else:
-                a = tl.load(
-                    a_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0
-                )
-                b = tl.load(
-                    b_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0
-                )
+    acc_dtype = tl.float32 if c_ptr.type.element_ty != tl.int8 else tl.int32
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=acc_dtype)
 
-            accumulator += tl.dot(a, b, input_precision="ieee")
+    for k in range(num_k_iter - 1):
+        # Load the next block of A and B, generate a mask by checking the K dimension.
+        # If it is out of bounds, set it to 0.
+        a = tl.load(a_ptrs)
+        b = tl.load(b_ptrs, cache_modifier=cache_modifier)
 
-            # Advance the ptrs to the next K block.
-            a_ptrs += BLOCK_SIZE_K * stride_ak
-            b_ptrs += BLOCK_SIZE_K * stride_bk
+        accumulator += tl.dot(a, b, input_precision="ieee")
 
-        c = accumulator.to(c_ptr.type.element_ty)
+        # Advance the ptrs to the next K block.
+        a_ptrs += (NUM_KSPLIT * BLOCK_SIZE_K) * stride_ak
+        b_ptrs += (NUM_KSPLIT * BLOCK_SIZE_K) * stride_bk
 
-        # Write back the block of the output matrix C with masks.
-        offs_cm = pid_m.to(tl.int64) * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
-        offs_cn = pid_n.to(tl.int64) * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-        c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
-        c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
-        if NUM_KSPLIT == 1:
-            tl.store(c_ptrs, c, mask=c_mask)
-        else:
-            tl.atomic_add(c_ptrs, c, mask=c_mask, sem="relaxed")
+    a = tl.load(
+        a_ptrs, mask=offs_k[None, :] < K - (num_k_iter - 1) * (NUM_KSPLIT * BLOCK_SIZE_K), other=0.0
+    )
+    b = tl.load(
+        b_ptrs, mask=offs_k[:, None] < K - (num_k_iter - 1) * (NUM_KSPLIT * BLOCK_SIZE_K), other=0.0
+    )
+    accumulator += tl.dot(a, b, input_precision="ieee")
+
+    c = accumulator.to(c_ptr.type.element_ty)
+
+    # Write back the block of the output matrix C with masks.
+    offs_cm = pid_m.to(tl.int64) * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_cn = pid_n.to(tl.int64) * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
+    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+    if NUM_KSPLIT == 1:
+        tl.store(c_ptrs, c, mask=c_mask)
+    else:
+        tl.atomic_add(c_ptrs, c, mask=c_mask, sem="relaxed")
 
 
 @functools.lru_cache(maxsize=1024)
@@ -155,22 +153,35 @@ def _get_config(
                 _get_config._config_dict[key] = config
         else:
             key = "default"  # fall back to default config
-            # single config. for the default path
-            return _get_config._config_dict[key]["any"]
-    if M < 32:
+
+    if key == "default":
+        key = f"{M}_{N}"
+        if key not in _get_config._config_dict.keys():
+            dev = arch_info.get_device()
+            fpath = f"{AITER_TRITON_CONFIGS_PATH}/gemm/{dev}-GEMM-A16W16-ATOMIC-M={M}-N={N}.json"
+            if os.path.exists(fpath):
+                with open(fpath, "r") as file:
+                    config = json.load(file)
+                    _get_config._config_dict[key] = config
+            else:
+                key = "default"  # fall back to default config
+
+    if M < 32 and "small" in _get_config._config_dict[key]:
         return _get_config._config_dict[key]["small"]
-    elif M <= 128:
+    elif M >= 32 and M <= 128 and ("medium_M32" in _get_config._config_dict[key] or "medium_M64" in _get_config._config_dict[key] or "medium_M128" in _get_config._config_dict[key]):
         BLK_M = triton.next_power_of_2(M)
-        if BLK_M == 32:
+        if BLK_M == 32 and "medium_M32" in _get_config._config_dict[key]:
             return _get_config._config_dict[key]["medium_M32"]
-        elif BLK_M == 64:
+        elif BLK_M == 64 and "medium_M64" in _get_config._config_dict[key]:
             return _get_config._config_dict[key]["medium_M64"]
-        elif BLK_M == 128:
+        elif BLK_M == 128 and "medium_M128" in _get_config._config_dict[key]:
             return _get_config._config_dict[key]["medium_M128"]
-    elif M <= 256:
+    elif M > 128 and M <= 256 and "large" in _get_config._config_dict[key]:
         return _get_config._config_dict[key]["large"]
-    else:
+    elif M > 256 and "xlarge" in _get_config._config_dict[key]:
         return _get_config._config_dict[key]["xlarge"]
+    else:
+        return _get_config._config_dict[key]["any"]
 
 
 def gemm_a16w16_atomic(
@@ -210,6 +221,8 @@ def gemm_a16w16_atomic(
         config["NUM_KSPLIT"] = 1
     if "cache_modifier" not in config:
         config["cache_modifier"] = ""
+    if "DISABLE_BUFFER_LOADS" not in config:
+        config["DISABLE_BUFFER_LOADS"] = False
 
     if y is None:
         # atomic add requires 0 tensor
@@ -223,10 +236,7 @@ def gemm_a16w16_atomic(
         * triton.cdiv(N, META["BLOCK_SIZE_N"])
         * META["NUM_KSPLIT"],
     )
-    # NOTE: if k split doesnt divide K evenly, this will waste compute
-    SPLITK_BLOCK_SIZE = triton.cdiv(K, config["NUM_KSPLIT"])
-    config["SPLITK_BLOCK_SIZE"] = SPLITK_BLOCK_SIZE
-    _gemm_a16_w16_atomic_kernel[grid](
+    kernel = _gemm_a16_w16_atomic_kernel[grid](
         x,
         w,
         y,
