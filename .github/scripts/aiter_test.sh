ddf4097fb86b2008d4581c56824424a4f2826689
Xin Huang <Xin.Huang@amd.com>
2025-08-11 19:55:55 +0800

    Add '-a' when logging

--- .github/scripts/aiter_test.sh
+++ .github/scripts/aiter_test.sh
@@ -16,7 +16,7 @@ fi
 for file in "${files[@]}"; do
     # Run each test file with a 30-minute timeout, output to latest_test.log
     echo "Running $file..."
-    timeout 30m bash -c "python3 \"$file\" 2>&1 | tee latest_test.log"
+    timeout 30m bash -c "python3 \"$file\" 2>&1 | tee -a latest_test.log"
 done
 
 
