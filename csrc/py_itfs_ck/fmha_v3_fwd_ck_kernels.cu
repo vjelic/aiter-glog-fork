887249ca6bd8f8eb172a2588bac540f1b3340fc0
PoYen, Chen <PoYen.Chen@amd.com>
2025-08-12 09:01:35 -0500

    Revert "Add missing pipeline template arg"
    This reverts commit 9ac5f47a24f9ff88fb8a153743201ac89b4319f2.

--- csrc/py_itfs_ck/fmha_v3_fwd_ck_kernels.cu
+++ csrc/py_itfs_ck/fmha_v3_fwd_ck_kernels.cu
@@ -3139,7 +3139,6 @@ struct get_kernel
         false, // kIsGroupMode
         fmha_variant,
         fmha_mask,
-        false, // kUseTrLoad
         fmha_traits>;
 
     using fmha_pipeline = aiter::BlockFmhaPipelineQRKSVS<fmha_problem>;
