eae3664160e2f37b7325f1616b9e048bbfce16ea
Xin Huang <Xin.Huang@amd.com>
2025-08-12 19:44:56 +0800

    Fix

--- .github/scripts/aiter_test.sh
+++ .github/scripts/aiter_test.sh
@@ -3,13 +3,13 @@ set -euo pipefail
 
 MULTIGPU=${MULTIGPU:-FALSE}
 
+MULTIGPU_TESTS=("test_communication.py" "test_custom_allreduce.py")
+
 files=()
 
 if [[ "$MULTIGPU" == "TRUE" ]]; then
-    # Recursively find all files under op_tests/multigpu_tests
-    mapfile -t files < <(find op_tests/multigpu_tests -type f -name "*.py")
+    mapfile -t files < <(find op_tests/multigpu_tests -type f \( -name "${MULTIGPU_TESTS[0]}" -o -name "${MULTIGPU_TESTS[1]}" \))
 else
-    # Recursively find all files under op_tests, excluding op_tests/multigpu_tests
     mapfile -t files < <(find op_tests -maxdepth 1 -type f -name "*.py")
 fi
 
--- .github/scripts/build_triton.sh
+++ .github/scripts/build_triton.sh
@@ -3,15 +3,14 @@
 set -ex
 
 # Install dependencies
-pip install --upgrade pip
-pip install pandas zmq einops numpy==1.26.2
+pip install --upgrade pandas zmq einops numpy==1.26.2
 python3 setup.py develop
 
 # Install aiter
 pip uninstall -y triton || true
 
 # Clone the triton repository and install it
-git clone --depth 1 https://github.com/triton-lang/triton || true
+git clone --depth=1 https://github.com/triton-lang/triton || true
 cd triton
 pip install -r python/requirements.txt
 pip install filecheck
